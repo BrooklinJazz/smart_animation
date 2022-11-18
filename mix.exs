8832948456a5f4dbc48c0a3f6c8b7e69c74986fc
Brooklin <brooklinjazzmyers@gmail.com>
2022-11-17 23:30:35 -0800

    update kino version

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule SmartAnimation.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:kino, "~> 0.6.2"}
+      {:kino, "~> 0.7"}
     ]
   end
 end
